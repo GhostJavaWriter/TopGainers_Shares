e986f55d92e19164f0ac80941634fbd6f4c128b4
Bair Nadtsalov <nadtsalov@gmail.com>
2021-01-30 08:55:03 +0800

    initial commit

---
+++ TinkoffLab_shares/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TinkoffLab_shares
+//
+//  Created by Bair Nadtsalov on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
